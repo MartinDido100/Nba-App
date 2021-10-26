09314d9d6fdb87a82cc1bc92a00ab55265738600
Martin Didolich <marindidolich2012@gmail.com>
2021-10-26 16:42:09 -0300

    aa

--- backend/db/config.js
+++ backend/db/config.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 
 const dbConnection = async () => {
+    console.log(typeof process.env.DB_CNN)  
     try {
         await mongoose.connect(process.env.DB_CNN,{
             useNewUrlParser : true,
